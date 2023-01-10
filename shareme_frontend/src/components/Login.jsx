7530d166d47bb50e9fc8fb60b222141f5bcd25f2
Gerrishonngugi <gerrishonngugi@gmail.com>
2023-01-10 10:53:24 +0300

    Login page components

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle} from 'react-icons/fc'; 
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
 const login = () => {
   return (
-    <div>login</div>
+    <div className='flex justify-start items-center flex-col h-screen'>login</div>
   )
 }
 
